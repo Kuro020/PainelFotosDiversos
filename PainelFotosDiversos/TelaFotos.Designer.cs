8c06d8c03231c8efda3e40a8dc7ca01e9b9ecfcb
Kuro <runescapeshiro02@gmail.com>
2022-04-19 16:43:40 -0300

    Adicionar arquivos de projeto.

---
+++ PainelFotosDiversos/TelaFotos.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace PainelFotosDiversos
+{
+    partial class TelaFotos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlFoto2 = new System.Windows.Forms.Panel();
+            this.pbxFoto2 = new System.Windows.Forms.PictureBox();
+            this.pnlFoto1 = new System.Windows.Forms.Panel();
+            this.pbxFoto1 = new System.Windows.Forms.PictureBox();
+            this.pnlFoto3 = new System.Windows.Forms.Panel();
+            this.pbxFoto3 = new System.Windows.Forms.PictureBox();
+            this.btnFoto1 = new System.Windows.Forms.Button();
+            this.btnFoto2 = new System.Windows.Forms.Button();
+            this.btnFoto3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pnlFoto2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxFoto2)).BeginInit();
+            this.pnlFoto1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxFoto1)).BeginInit();
+            this.pnlFoto3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxFoto3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlFoto2
+            // 
+            this.pnlFoto2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pnlFoto2.Controls.Add(this.pbxFoto2);
+            this.pnlFoto2.Location = new System.Drawing.Point(48, 44);
+            this.pnlFoto2.Name = "pnlFoto2";
+            this.pnlFoto2.Size = new System.Drawing.Size(244, 351);
+            this.pnlFoto2.TabIndex = 0;
+            // 
+            // pbxFoto2
+            // 
+            this.pbxFoto2.Location = new System.Drawing.Point(18, 14);
+            this.pbxFoto2.Name = "pbxFoto2";
+            this.pbxFoto2.Size = new System.Drawing.Size(207, 321);
+            this.pbxFoto2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxFoto2.TabIndex = 0;
+            this.pbxFoto2.TabStop = false;
+            // 
+            // pnlFoto1
+            // 
+            this.pnlFoto1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pnlFoto1.Controls.Add(this.pbxFoto1);
+            this.pnlFoto1.Location = new System.Drawing.Point(318, 44);
+            this.pnlFoto1.Name = "pnlFoto1";
+            this.pnlFoto1.Size = new System.Drawing.Size(244, 351);
+            this.pnlFoto1.TabIndex = 1;
+            // 
+            // pbxFoto1
+            // 
+            this.pbxFoto1.Location = new System.Drawing.Point(19, 14);
+            this.pbxFoto1.Name = "pbxFoto1";
+            this.pbxFoto1.Size = new System.Drawing.Size(207, 321);
+            this.pbxFoto1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxFoto1.TabIndex = 1;
+            this.pbxFoto1.TabStop = false;
+            // 
+            // pnlFoto3
+            // 
+            this.pnlFoto3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pnlFoto3.Controls.Add(this.pbxFoto3);
+            this.pnlFoto3.Location = new System.Drawing.Point(587, 44);
+            this.pnlFoto3.Name = "pnlFoto3";
+            this.pnlFoto3.Size = new System.Drawing.Size(244, 351);
+            this.pnlFoto3.TabIndex = 2;
+            // 
+            // pbxFoto3
+            // 
+            this.pbxFoto3.Location = new System.Drawing.Point(20, 14);
+            this.pbxFoto3.Name = "pbxFoto3";
+            this.pbxFoto3.Size = new System.Drawing.Size(207, 321);
+            this.pbxFoto3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxFoto3.TabIndex = 2;
+            this.pbxFoto3.TabStop = false;
+            this.pbxFoto3.Click += new System.EventHandler(this.pbxFoto3_Click);
+            // 
+            // btnFoto1
+            // 
+            this.btnFoto1.Location = new System.Drawing.Point(406, 401);
+            this.btnFoto1.Name = "btnFoto1";
+            this.btnFoto1.Size = new System.Drawing.Size(75, 23);
+            this.btnFoto1.TabIndex = 3;
+            this.btnFoto1.Text = "Buscar";
+            this.btnFoto1.UseVisualStyleBackColor = true;
+            this.btnFoto1.Click += new System.EventHandler(this.btnFoto1_Click);
+            // 
+            // btnFoto2
+            // 
+            this.btnFoto2.Location = new System.Drawing.Point(134, 401);
+            this.btnFoto2.Name = "btnFoto2";
+            this.btnFoto2.Size = new System.Drawing.Size(75, 23);
+            this.btnFoto2.TabIndex = 4;
+            this.btnFoto2.Text = "Buscar";
+            this.btnFoto2.UseVisualStyleBackColor = true;
+            this.btnFoto2.Click += new System.EventHandler(this.btnFoto2_Click);
+            // 
+            // btnFoto3
+            // 
+            this.btnFoto3.Location = new System.Drawing.Point(679, 401);
+            this.btnFoto3.Name = "btnFoto3";
+            this.btnFoto3.Size = new System.Drawing.Size(75, 23);
+            this.btnFoto3.TabIndex = 5;
+            this.btnFoto3.Text = "Buscar";
+            this.btnFoto3.UseVisualStyleBackColor = true;
+            this.btnFoto3.Click += new System.EventHandler(this.btnFoto3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // TelaFotos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 450);
+            this.Controls.Add(this.btnFoto3);
+            this.Controls.Add(this.btnFoto2);
+            this.Controls.Add(this.btnFoto1);
+            this.Controls.Add(this.pnlFoto3);
+            this.Controls.Add(this.pnlFoto1);
+            this.Controls.Add(this.pnlFoto2);
+            this.Name = "TelaFotos";
+            this.Text = "TelaFotos";
+            this.pnlFoto2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxFoto2)).EndInit();
+            this.pnlFoto1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxFoto1)).EndInit();
+            this.pnlFoto3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxFoto3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlFoto2;
+        private System.Windows.Forms.Panel pnlFoto1;
+        private System.Windows.Forms.Panel pnlFoto3;
+        private System.Windows.Forms.PictureBox pbxFoto2;
+        private System.Windows.Forms.PictureBox pbxFoto1;
+        private System.Windows.Forms.PictureBox pbxFoto3;
+        private System.Windows.Forms.Button btnFoto1;
+        private System.Windows.Forms.Button btnFoto2;
+        private System.Windows.Forms.Button btnFoto3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
---
+++ PainelFotosDiversos/TelaFotos.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PainelFotosDiversos
+{
+    public partial class TelaFotos : Form
+    {
+        public TelaFotos()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFoto1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Arquivos png|*.png";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pbxFoto1.ImageLocation = openFileDialog1.FileName;
+            }
+        }
+
+        private void btnFoto2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Arquivos png|*.png";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pbxFoto2.ImageLocation = openFileDialog1.FileName;
+            }
+        }
+
+        private void btnFoto3_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Arquivos png|*.png";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pbxFoto3.ImageLocation = openFileDialog1.FileName;
+            }
+        }
+
+        private void pbxFoto3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
